bf052caa6759f5f6be7a8398f705ed7e83fa9540
Susu <etu45639@henallux.be>
2022-12-15 09:22:53 +0100

    Partie 2 suite fonction principale

--- partie 2/main.js
+++ partie 2/main.js
@@ -23,6 +23,61 @@ const tabPoisson = [0.183, 0.494, 0.758, 0.908, 0.9716,  0.9932,  0.99932, 1.000
 /*                      Code                    */
 /*----------------------------------------------*/
 
+// module de calcul du nombre de stations optimal
+function nbStationsOptimal(nbStationsMin, nbStationsMax, tempsSimu) {
+    for( let nbStations = nbStationsMin; nbStations <= nbStationsMax; nbStations++){
+        let file = 0,
+            filePrioritaire = 0,
+            fileCumulée = 0,
+            filePrioritaireCumulée = 0,
+            nbTransfoClientTot = 0,
+            tempsInnocupéTot = 0;
+        let stations = [];
+        initStations(nbStations, stations);
+
+        for(let temps = 1; temps <= tempsSimu; temps++){
+            // affichage des états de chawue stations pour les 20 premières minutes du nombre de station min
+            if( temps <= 20 && nbStations == nbStationsMin ){
+                console.log("AVANT PLACEMENT");
+                for(let i = 0; i < nbStations; i++){
+                    let message =  "- Stations "+ (i+1) +" : ";
+                    if(stations[i] !== 0){
+                        message += stations[i] +" clients.";
+                    }
+                    else{
+                        message += "pas de client.";
+                    }
+                    console.log(message);
+                }
+                // affichage de la description de chaque file
+                console.log("File de client ordinaire : "+ fileOrdinaire +" client(s).");
+                console.log("File de client prioritaire :"+ filePrioritaire +" client(s) (max : 5)");
+            }  
+            // génération des arrivées
+            const {nbArrivées, x0N :x01} = génèreArrivées(x0, a, c, m, tabLoiPoisson);
+            x0 = x01;
+            // répartition des arrivées entre prioritaire et ordinaire
+            const {fileOrdinaire, filePrio, x0N: x02, nbTransformation} = répartitionArrivée(file, filePrioritaire, x0, nbTransformation);
+            // ajoute les clients générés à la file correspondante
+            file = fileOrdinaire;
+            filePrioritaire += filePrio;
+            nbTransfoClientTot += nbTransformation;
+            x0 = x02;
+
+            // affichage pour les 20 première minutes du nombre de station min
+            if( temps <= 20 && nbStations === nbStationsMin ){
+                console.log('Minutes '+ temps +' : '+ nbArrivées +' arrivées générées.');
+                console.log(fileOrdinaire +' clients ordinaires et '+ filePrio +' clients prioritaires.');
+            }
+
+            // répartition des clients prioritaire dans la première station
+            const {filePrio: filePrioNew, x0N: x03, tempsInnocupé} = repartiClientPrio(stations, filePrioritaire, x0);
+            x0 = x03;
+            tempsInnocupéTot += filePrioNew;
+            filePrioritaire = filePrioNew;
+        }
+    }
+}
 
 /*  
 
@@ -30,58 +85,7 @@ const tabPoisson = [0.183, 0.494, 0.758, 0.908, 0.9716,  0.9932,  0.99932, 1.000
  │ nbStationsOptimal │
  o───────────────────o ↓ nbStationsOptimal
 ┌─── *
-│ nbStations = nbStationsMin
-│╔══ while (nbStations ≤ nbStationsMax)
-│║ file = 0
-│║ filePrioritaire = 0
-│║ fileCumulée = 0
-│║ filePrioCumulée = 0
-│║ nbTransfoClientTot = 0
-│║ tempsInnocupéTot = 0
-│║ 
-│║ o──────────────o ↓ nbStations
-│║ │ initStations │
-│║ o──────────────o ↓ stations
-│║ temps = 1
-│║ 
-│║╔══ while (temps ≤ tempsSimul)
-│║║ // affichage des états de chaque stations pour les 20 premières minutes du nombre de station min
-│║║┌── if( temps ≤ 20 AND nbStattions == nbStationsMin )
-│║║│ sortir "AVANT PLACEMENT"
-│║║│ i = 0
-│║║│╔══ while ( i < nbStations )
-│║║│║ sortir " - Station "+ i +" : "
-│║║│║┌── if( station[i] ≠ 0)
-│║║│║│ // ajouté le type de client
-│║║│║│ sortir stations[i] +" clients."
-│║║│║├── else
-│║║│║│ sortir " pas de client."
-│║║│║└──
-│║║│║ i++
-│║║│╙──
-│║║│ 
-│║║│ // affichage de la description de chaque file
-│║║│ // infos nécessaire pour la compréhension des files ?
-│║║│ sortir "File de client ordinaire : "+ fileOrdinaire +" client(s)."
-│║║│ sortir "File de client prioritaire :"+ filePrioritaire +" client(s) (max : 5)"
-│║║└──
-│║║ 
-│║║ o──────────────────o ↓ x0, a, c, m, tabLoiPoisson
-│║║ │ nbArrivéesGénéré │
-│║║ o──────────────────o ↓ nbArrivées, x0;
-│║║ o────────────────────o ↓  nbArrivées, x0, a, c, m
-│║║ │ répartitionArrivée │
-│║║ o────────────────────o ↓  fileOrdinaire, filePrio, x0, nbTransformation
-│║║ nbTransfoClientTot += nbTransformation
-│║║ file += fileOrdinaire
-│║║ filePrioritaire += filePrio
-│║║ 
-│║║┌── if(nbStattions == nbStationsMin AND temps ≤ 20 )
-│║║│ sortir "Minutes "+ temps +" : "+ nbArrivées +" arrivées générées."
-│║║│ // durée ??
-│║║│ sortir fileOrdinaire +" clients ordinaires et "+ filePrio +" clients prioritaires."
-│║║└──
-│║║ 
+
 │║║ o───────────────────o ↓  stations, x0, a, c, m, tempsInnocupé
 │║║ │ repartiClientPrio │
 │║║ o───────────────────o ↓  stations, filePrioritaires, x0, temspInnocupé
@@ -190,13 +194,14 @@ function répartitionArrivée(nbArrivées, x0, a, c, m){
 }
 
 // traitement du client prioritaire
-function repartiClientPrio(stations, filePrioritaire, x0, tempsInnocupé){
+function repartiClientPrio(stations, filePrio, x0){
+    let tempsInnocupé = 0;
     // regarde si la station est libre
     if(stations[0] === 0){
         // si elle est libre, regarde si un client attend dans la file
-        if(filePrioritaire !== 0){
+        if(filePrio !== 0){
             // génère le temps pour le client, l'enlève de la file et l'ajoute dans la station
-            filePrioritaire--;
+            filePrio--;
             x0 = génèreDurée(x0, a, c, m, stations, 0);
             // enlève une minute de traitement
             stations[0]--;
@@ -209,17 +214,17 @@ function repartiClientPrio(stations, filePrioritaire, x0, tempsInnocupé){
         stations[0]--;
     }
     // stations passé par référence (pas besoin de le retourner)
-    return {x0, filePrioritaire, tempsInnocupé};
+    return {x0, filePrio, tempsInnocupé};
 }
 
 // réparti les clients dans les stations
-function repartiClient(stations, file, x0, a, c, m, tempsInnocupé){
+function repartiClient(stations, file, x0, a, c, m){
     // parcour le tableau de station pour
     // - regarder si une station est libre (== 0)
     // => si oui, génère une durée pour un client si il attend dans la file 
     //      , l'ajoute à la station et enlève un client de la file
     // - enlève une minute de traitement de chaque station
-
+    let tempsInnocupé = 0;
     // pas 0 car la station 0 est résevé au client prioritaire
     for(let i = 1; i < stations.length; i++){
         if(stations[i] === 0){
